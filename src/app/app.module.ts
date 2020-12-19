7e4f28815bcca9b6e01eab7fb132d1a1369fe965
andres <andres06energy@hotmail.com>
2020-12-19 02:41:35 -0300

    TestInicialWebApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+import { CardComponent } from './card/card.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarrouselComponent,
+    CardComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
